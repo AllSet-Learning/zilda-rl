1f7448f49d0037b29451f70f30c38645eb28847d
Jack B. Du <jd2804@nyu.edu>
2014-10-11 11:55:32 +0800

    map rearranged, light removed

--- js/basic.js
+++ js/basic.js
@@ -2,41 +2,41 @@
 var game = new RL.Game();
 
 
-RL.Entity.Types.next = {
-		name: 'next',
-		char: ',',
-		color: 'white',
-		bgColor: false,
-		bump: function(entity){
-			// if bumping entity is the player
-			if(entity === this.game.player){
-				// @TODO combat logic here
-				mapData = mapData1;
-				game = new RL.Game();
-				gameReady();
-				game.console.log('You are entering next level');
-				game.start();
-				
-			}
-			return false;
-		}
-};
+// RL.Entity.Types.next = {
+// 		name: 'next',
+// 		char: ',',
+// 		color: 'white',
+// 		bgColor: false,
+// 		bump: function(entity){
+// 			// if bumping entity is the player
+// 			if(entity === this.game.player){
+// 				// @TODO combat logic here
+// 				mapData = mapData1;
+// 				game = new RL.Game();
+// 				gameReady();
+// 				game.console.log('You are entering next level');
+// 				game.start();
+// 				
+// 			}
+// 			return false;
+// 		}
+// };
 
 var mapData0 = [
-    "#########################",
+    "########################",
     "#...........#..........#",
-    "#...........#..........#",
+    "#..2........#....3.....#",
     "#...........#..........#",
     "#.....##+#####+###.....#",
-    "#.....#..........#.....#",
+    "#.....#..qwer....#.....#",
     "#.....#..........#.....#",
     "#######..........#######",
     "#.....#..........#.....#",
-    "#.....#+#######+##.....#",
+    "#..1..##+######+##.....#",
     "#...........#..........#",
+    "#...........#.....4....#",
     "#...........#..........#",
-    "#...........#..........#",
-    "#########################",
+    "########################",
 ];
 
 
@@ -83,7 +83,11 @@ var mapData = mapData0;
 var mapCharToType = {
     '#': 'wall',
     '.': 'floor',
-    '+': 'door'
+    '+': 'door',
+    'q': 'verifier1',
+    'w': 'verifier2',
+    'e': 'verifier3',
+    'r': 'verifier4',
 };
 
 var entityCharToType = {
@@ -92,7 +96,11 @@ var entityCharToType = {
     'C': 'zhong',
     'W': 'wen',
 	'F': 'flee',
-	',': 'next'
+	',': 'next',
+	'1': 'first',
+	'2': 'second',
+	'3': 'third',
+	'4': 'fourth'
 };
 
 var keyBindings = {
@@ -109,8 +117,10 @@ function gameReady() {
 	game.entityManager.loadEntitiesFromArrayString(mapData, entityCharToType);
 
 	// add some lights
-	game.lighting.set(3, 7, 255, 0, 0);
-	game.lighting.set(7, 7, 0, 0, 255);
+	game.lighting.set(10, 5, 255, 0, 0);
+	game.lighting.set(11, 5, 0, 0, 255);
+	game.lighting.set(12, 5, 0, 0, 255);
+	game.lighting.set(13, 5, 0, 0, 255);
 
 	// generate and assign a map object (repaces empty default)
 	game.setMapSize(game.map.width, game.map.height);
@@ -119,21 +129,21 @@ function gameReady() {
 	game.input.addBindings(keyBindings);
 
 	// create entities and add to game.entityManager
-	var entZombie = new RL.Entity(game, 'zombie');
-	game.entityManager.add(2, 8, entZombie);
-
-	// or just add by entity type
-	game.entityManager.add(5, 9, 'zombie');
-
-	game.entityManager.add(3, 11, 'next');
+// 	var entZombie = new RL.Entity(game, 'zombie');
+// 	game.entityManager.add(2, 8, entZombie);
+// 
+// 	// or just add by entity type
+// 	game.entityManager.add(5, 9, 'zombie');
+// 
+// 	game.entityManager.add(3, 11, 'next');
 
 	// set player starting position
 	game.player.x = 3;
 	game.player.y = 3;
 
-	game.lighting.set(20, 1, 0, 255, 0);
-	game.lighting.set(7, 14, 0, 0, 255);
-	game.lighting.set(20, 12, 255, 255, 255);
+// 	game.lighting.set(20, 1, 0, 255, 0);
+// 	game.lighting.set(7, 14, 0, 0, 255);
+// 	game.lighting.set(20, 12, 255, 255, 255);
 
 	// make the view a little smaller
 	game.renderer.resize(10, 14);
@@ -153,5 +163,6 @@ function gameReady() {
 
 game.console.log('The game starts.');
 // start the game
+console.log(game.entityManager.entities[3]);
 game.start();
 
